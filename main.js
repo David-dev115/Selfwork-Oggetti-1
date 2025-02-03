e67fc6a25f1066ddc6de66838684a1b4052cd065
David Paparo <d.paparo@tiscali.it>
2025-02-03 16:50:41 +0100

    carico esercizio svolto

--- main.js
+++ main.js
@@ -8,3 +8,28 @@
 // eta'
 // un metodo che permetta di salutare
 
+
+let persona = {
+    
+    'nome' : `David`,
+    'cognome' : `Paparo`,
+    'età' : 37,
+    'saluta_familiari' : function (familiare) {
+        
+        if (this.conoscenze.includes(familiare)) {
+            console.log(`Ciao ${familiare}`);
+        } else {
+            console.log(`Non posso salutare ${familiare}, perchè non lo conosco`);
+            
+        }
+        
+    },
+    'conoscenze' : [`Mariana`, `Giorgia`, `Giada`, `Riccardo`],
+    
+}
+
+
+persona.saluta_familiari(`Giada`);
+
+persona.saluta_familiari(`Piero`);
+
